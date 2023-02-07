dcf6b5150ee9c77dbccf0119aeb6b62a146072ee
Lê Đắc Hòa <Ledachoa99@gmail.com>
2023-02-07 11:46:20 +0700

    update (store): fetchAllSuggestions

--- src/stores/suggestion-store.js
+++ src/stores/suggestion-store.js
@@ -1,21 +1,26 @@
-import axios from 'axios'
-import { defineStore } from 'pinia'
+import axios from "axios";
+import { defineStore } from "pinia";
 
-export const useSuggestionStore = defineStore('suggestion', {
+export const useSuggestionStore = defineStore("suggestion", {
   state: () => ({
     suggestions: null,
+    all_suggestions: null,
   }),
   actions: {
     async fetchSuggestions() {
-        let res = await axios.get('api/suggestions')
-        this.$state.suggestions = res.data
+      let res = await axios.get("api/suggestions");
+      this.$state.suggestions = res.data;
+    },
+    async fetchAllSuggestions() {
+      let res = await axios.get("api/list-suggestions");
+      this.$state.all_suggestions = res.data;
     },
     suggestionImage(image) {
-      return import.meta.env.VITE_APP_API_URL + '/images/suggestions/' + image
+      return import.meta.env.VITE_APP_API_URL + "/images/suggestions/" + image;
     },
     clearSuggestion() {
-        this.$state.suggestions = null
-    }
+      this.$state.suggestions = null;
+    },
   },
-  persist: true
-})
+  persist: true,
+});
