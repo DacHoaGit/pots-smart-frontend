a06e5bbd7e022cf468ab4e64033b6b4faca8e765
Lê Đắc Hòa <Ledachoa99@gmail.com>
2023-02-07 11:20:07 +0700

    update

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap-utilities.min.css';
 import './axios.js'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { Button,Drawer, List, Menu,Progress, Card,Divider,Upload,TimePicker,Switch,DatePicker, Table, Rate ,Tooltip, Avatar,Image, Input, Form, Select } from 'ant-design-vue';
+import { Button,Drawer,Tabs,Tag, List,Space,Carousel,Col, Menu,Progress, Card,Divider,Upload,TimePicker,Switch,DatePicker, Table, Rate ,Tooltip, Avatar,Image, Input, Form, Select,SelectOptGroup, SelectOption } from 'ant-design-vue';
 import VueChartkick from 'vue-chartkick';
 import 'chartkick/chart.js';
 
@@ -25,15 +25,22 @@ app.use(VueChartkick)
 app.use(pinia)
 app.use(router)
 app.use(Button)
+app.use(Tabs)
+app.use(Tag)
+app.use(Carousel)
 app.use(Rate)
 app.use(TimePicker)
 app.use(DatePicker)
 app.use(Switch)
 app.use(Tooltip)
+app.use(Col)
 app.use(Upload)
+app.use(Space)
 app.use(Table)
 app.use(Divider)
 app.use(Select)
+app.use(SelectOptGroup)
+app.use(SelectOption)
 app.use(Progress)
 app.use(Form)
 app.use(Input)
