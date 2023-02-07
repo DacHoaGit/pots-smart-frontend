905e3e84e98f1c1b54c7c8e9838de80ed3c8eb6f
Lê Đắc Hòa <Ledachoa99@gmail.com>
2023-02-07 11:41:54 +0700

    add section: hero, question, feature, video, footer

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,493 @@
+<template>
+  <div class="hero-section">
+    <div class="hero-content">
+      <h1 class="display-4 font-weight-bold text-center">
+        Chậu Cây Thông Minh
+      </h1>
+      <p class="lead">
+        Thiết kế để tự động giữ gìn sức khoẻ và sắc đẹp của cây cảnh trong môi
+        trường nhà bạn.
+      </p>
+    </div>
+  </div>
+
+  
+
+  <div
+    style="background-color: rgba(0, 128, 128, 0.1)"
+    class="section-question"
+  >
+    <div
+      id="question"
+      style="display: flex; justify-content: center; flex-wrap: wrap"
+      class="question-poser"
+    >
+      <h2 style="margin-top: 48px; font-weight: bold" class="">
+        <img src="../../assets/question.png" alt="" />
+        Tại sao nên lựa chọn chậu cây thông minh ?
+      </h2>
+    </div>
+
+    <div style="display: flex; flex-wrap: wrap">
+      <div class="card">
+        <div class="card-icon">
+          <img src="../../assets/automation.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Tự động canh tác</h5>
+          <p class="card-text">
+            Chậu cây thông minh có thể tự động canh tác cho cây trồng, giúp bạn
+            tiết kiệm thời gian và công sức trong việc chăm sóc cây.
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-icon">
+          <img src="../../assets/control.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Điều chỉnh cảm biến</h5>
+          <p class="card-text">
+            Chậu cây thông minh có thể đo và điều chỉnh độ ẩm cho cây trồng,
+            giúp cây trồng không bị quá ướt hoặc quá khô.
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-icon">
+          <img src="../../assets/supervise.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Giám sát sức khỏe</h5>
+          <p class="card-text">
+            Chậu cây thông minh có thể giám sát sức khỏe của cây trồng và cung
+            cấp thông tin cho bạn về nắng, nhiệt độ và độ ẩm.
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-icon">
+          <img src="../../assets/save-water.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Tiết kiệm tài nguyên</h5>
+          <p class="card-text">
+            Chậu cây thông minh có thể tự động tính toán và tiết kiệm nước cho
+            cây trồng, giúp bạn tiết kiệm chi phí nước.
+          </p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-icon">
+          <img src="../../assets/pots3.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Thiết kế đẹp</h5>
+          <p class="card-text">
+            Chậu cây thông minh có thể được thiết kế với nhiều mẫu mã đẹp, phù
+            hợp với không gian nội thất của bạn.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div
+    id="feature"
+    style="background-color: rgba(184, 222, 141, 0.3)"
+    class="section-question"
+  >
+    <div
+      style="display: flex; justify-content: center; flex-wrap: wrap"
+      class="question-poser"
+    >
+      <h2 style="margin-top: 48px; font-weight: bold" class="">
+        <img src="../../assets/feature.png" alt="" />
+        Cung cấp các chức năng
+      </h2>
+    </div>
+
+    <div style="display: flex; flex-wrap: wrap" class="">
+      <div style="height: 500px" class="card">
+        <div class="card-icon">
+          <img src="../../assets/Humidity.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hiển thị cảm biến</h5>
+          <p class="card-text">
+            Tính năng đo và hiển thị các thông số về nhiệt độ, độ ẩm đất và độ
+            ẩm không khí trong môi trường. Nó giúp người dùng biết được tình
+            trạng hiện tại và dễ dàng quản lý các yếu tố môi trường như nhiệt
+            độ, độ ẩm đất và độ ẩm không khí cho một môi trường hoàn hảo cho các
+            cây, hoa và các loài sinh vật khác. Tính năng này cần thiết đối với
+            những ai quan tâm đến sức khoẻ và sự phát triển của các cây trồng,
+            hoa và động vật trong môi trường.
+          </p>
+        </div>
+      </div>
+
+      <div style="height: 500px" class="card">
+        <div class="card-icon">
+          <img src="../../assets/control.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Điều khiển đèn và bơm</h5>
+          <p class="card-text">
+            Tính năng điều khiển điện một cách tự động cho các thiết bị như đèn
+            và bơm nước. Nó có thể được cấu hình để tự động bật/tắt hoặc điều
+            chỉnh mức độ sáng/lưu lượng bơm nước theo thời gian và điều kiện
+            nhất định. Tính năng này giúp giảm tiêu thụ điện và nước, giảm rủi
+            ro về an toàn điện và giảm tải cho người dùng.
+          </p>
+        </div>
+      </div>
+
+      <div style="height: 500px" class="card">
+        <div class="card-icon">
+          <img src="../../assets/search.png" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Nhận diện bệnh trên cây</h5>
+          <p class="card-text">
+            Sử dụng công nghệ hình ảnh và machine learning để phát hiện bất kỳ
+            dấu hiệu nhạy cảm nào của bệnh trên cây. Upload hình ảnh và so sánh
+            với các hình ảnh của cây và bệnh từ cơ sở dữ liệu. Nếu phát hiện bất
+            kỳ dấu hiệu nhạy cảm nào, nó sẽ gửi thông báo cho người sử dụng và
+            cung cấp hướng dẫn cách xử lý bệnh.Giúp người dùng dễ dàng phát hiện
+            và xử lý sớm các vấn đề về sức khoẻ của cây, giúp cây phát triển
+            mạnh mẽ và tươi trẻ hơn.
+          </p>
+        </div>
+      </div>
+
+      <div style="height: 500px" class="card">
+        <div class="card-icon">
+          <img src="../../assets/chart.gif" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Biểu đồ</h5>
+          <p class="card-text">
+            Tính năng hiển thị biểu đồ theo dõi thông số nhiệt độ, độ ẩm đất và
+            độ ẩm không khí dựa trên dữ liệu thời gian thực được ghi lại bởi
+            thiết bị đo nhiệt độ, độ ẩm và độ ẩm không khí trong chậu cây. Biểu
+            đồ này có thể hiển thị theo đồ thị dạng đường, cho phép người dùng
+            dễ dàng theo dõi và đánh giá sự thay đổi của các thông số trong thời
+            gian dài. Tính năng này giúp người dùng đưa ra quyết định hợp lý về
+            việc quản lý và bảo dưỡng cây, đảm bảo sức khoẻ của cây và sự phát
+            triển tốt nhất của nó.
+          </p>
+        </div>
+      </div>
+
+      <div style="height: 500px" class="card">
+        <div class="card-icon">
+          <img src="../../assets/timer.gif" alt="" />
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Hẹn giờ</h5>
+          <p class="card-text">
+            Tính năng hẹn giờ bật tắt thiết bị cho phép người sử dụng đặt lịch
+            bật hoặc tắt thiết bị tự động theo thời gian đã đặt trước. Tính năng
+            này cải thiện hiệu quả và tiện lợi trong việc quản lý thiết bị và
+            giúp bảo vệ môi trường.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div
+    id="video"
+    style="background-color: rgba(184, 222, 141, 0.3)"
+    class="section-question"
+  >
+    <div
+      style="display: flex; justify-content: center; flex-wrap: wrap"
+      class="question-poser"
+    >
+      <h2 style="margin-top: 48px; font-weight: bold" class="">
+        <img src="../../assets/video.png" alt="" />
+        Video về sản phẩm
+      </h2>
+    </div>
+
+    <div style="display: flex; flex-wrap: wrap" class="">
+      <div v-for="video in videoStore.videos" style="height: auto" class="card">
+        <div class="card-icon">
+          <iframe height="300"  allowfullscreen="" frameborder="0" w :src="`https://www.youtube.com/embed/${video.link}?autoplay=0`"></iframe>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ video.title }}</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-content">
+        <h3 class="footer-title">
+          <img class="" style="" src="../../assets/logo.svg" alt="logo" />
+        </h3>
+        <p class="footer-text">
+          Cung cấp giải pháp về quản lý cây với công nghệ tiên tiến
+        </p>
+        <nav class="footer-nav">
+          <ul class="footer-nav-list">
+            <li>
+              <a href="#"
+                ><img class="" src="../../assets/facebook.png" alt="logo"
+              /></a>
+            </li>
+
+            <li>
+              <a href="#"
+                ><img class="" src="../../assets/twitter.png" alt="logo"
+              /></a>
+            </li>
+
+            <li>
+              <a href="#"
+                ><img class="" src="../../assets/instagram.png" alt="logo"
+              /></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="footer-contact">
+        <h3 class="footer-contact-title">Liên hệ</h3>
+        <p class="footer-contact-text">Địa chỉ: Thanh Xuân, Hà Nội</p>
+        <p class="footer-contact-text">Điện thoại: +84 999 999 999</p>
+        <p class="footer-contact-text">Email: Ledachoa01@gmail.com</p>
+      </div>
+    </div>
+    <p class="footer-copyright">Copyright &copy; 2023 Chậu cây thông minh</p>
+  </footer>
+</template>
+
+<script setup>
+import { ref,onMounted } from "vue";
+import { useVideoStore } from "../../stores/video-store";
+
+const videoStore = useVideoStore();
+onMounted(async () => {
+    await videoStore.fetchVideos();
+
+});
+
+</script>
+
+<style scoped>
+.hero-section {
+  background-image: url(../../assets/bg.jpg);
+  background-size: cover;
+  background-position: center;
+  height: 900px;
+  display: flex;
+  width: 100%;
+  align-items: center;
+}
+.hero-content {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  background-color: rgba(255, 255, 255, 0.3);
+  height: 100%;
+}
+.hero-content p {
+  width: 40%;
+  font-size: 32px;
+  font-weight: 800;
+  line-height: 1.8em;
+  letter-spacing: 0.5px;
+  font-family: "Work Sans";
+  color: #0b2a28;
+  text-align: center;
+}
+.hero-content h1 {
+  width: 100%;
+  font-family: "Work Sans";
+  font-weight: 700;
+  font-size: 500%;
+  margin: 0;
+  padding: 0;
+  line-height: 500%;
+  color: #003e36;
+}
+
+.question-poser {
+  text-align: center;
+  font-family: "Work Sans";
+  font-size: 24px;
+}
+
+@media (max-width: 768px) {
+  .hero-section {
+    height: 400px;
+    position: relative;
+  }
+  .hero-content {
+    display: block;
+    position: absolute;
+    top: 50px;
+  }
+  .hero-content h1 {
+    color: #003e36;
+    font-family: "Work Sans";
+    font-size: 20px;
+    font-weight: 600;
+    letter-spacing: 0.5px;
+    margin-top: 50px;
+  }
+  .hero-content p {
+    font-size: 16px;
+    width: 100%;
+    text-align: center;
+  }
+  .question-poser h2 {
+    font-size: 28px;
+  }
+  .question-poser img {
+    width: 40px;
+  }
+}
+
+.card {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0px 2px 10px #ccc;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  margin: 80px auto;
+  text-align: center;
+  transition: all 0.3s ease-in-out;
+}
+
+.card:hover {
+  transform: scale(1.1);
+  box-shadow: 0px 5px 20px #ccc;
+  cursor: pointer;
+}
+
+.card-icon {
+  font-size: 60px;
+  color: #5b5b5b;
+  margin-top: 20px;
+}
+
+.card-body {
+  padding: 20px;
+}
+
+.card-title {
+  font-weight: bold;
+  font-size: 20px;
+  color: #333;
+  margin-top: 10px;
+}
+
+.card-text {
+  font-size: 14px;
+  color: #333;
+  margin-top: 10px;
+  text-align: justify;
+}
+
+@media (max-width: 767px) {
+  .card {
+    width: 90%;
+    height: auto;
+    margin: 10px auto;
+  }
+
+  .img-logo {
+    width: 100px;
+  }
+
+  .footer-content {
+    width: 50%;
+  }
+  .footer-nav-list img {
+    width: 30px;
+  }
+  .footer-content {
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 12px;
+    margin-right: 2px;
+  }
+  .footer-contact {
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 12px;
+    margin-left: 2px;
+  }
+}
+.footer-nav-list li:hover {
+  background-color: #4c9e9e;
+  transition: 0.3s ease;
+}
+.footer-nav-list li {
+  border-radius: 16px;
+  margin: 0 12px 0 0;
+}
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 40px 0;
+}
+.footer-nav-list img {
+  width: 50px;
+}
+
+.footer-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.footer-title img {
+  width: 150px;
+}
+
+.footer-title,
+.footer-contact-title {
+  color: #fff;
+  font-size: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.footer-text,
+.footer-contact-text {
+  font-size: 14px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.footer-nav {
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: center;
+}
+
+.footer-nav-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+}
+
+.footer-copyright {
+  font-size: 14px;
+  color: gray;
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
