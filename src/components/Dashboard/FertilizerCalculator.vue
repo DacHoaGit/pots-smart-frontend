66e37fc5ce2f03aa66a07462c32d00a9e657f307
Lê Đắc Hòa <Ledachoa99@gmail.com>
2023-02-07 11:24:10 +0700

    remove

--- src/components/Dashboard/FertilizerCalculator.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <a-card title="Tính toán phân bón" style="width: 100"></a-card>
-</template>
-
-<script setup>
-import { useMenu } from "../../stores/menu-store";
-useMenu().onSelectedKeys(["FertilizerCalculator"]);
-useMenu().onOpenKeys(["FertilizerCalculator"]);
-</script>
